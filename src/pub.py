d95013f0b7523f8837e946124561b6ff6b40a963
Joshua <jssmiller94@gmail.com>
2021-04-07 15:17:35 +0100

    some advanced extensions finished

--- src/pub.py
+++ src/pub.py
@@ -1,9 +1,10 @@
 class Pub:
 
-    def __init__(self, name: str, till: float, drinks: list):
+    def __init__(self, name: str, till: float, drinks: list, food):
         self.name =  name
         self.till = till
         self.drinks = []
+        self.food = []
 
     def sell_drink(self, drink, customer):
         if (customer.age >= 18) & (customer.drunkeness <= 6):
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -2,11 +2,15 @@ import unittest
 from src.pub import Pub
 from src.drink import Drink
 from src.customer import Customer
+from src.food import Food
 
 class TestPub(unittest.TestCase):
     
     def setUp(self):
-        self.pub = Pub("The Prancing Pony", 100.00, ["beer", "cider"])
+        self.food1 = Food("Chips", 2.50, 5)
+        self.food2 = Food("Nachos", 3, 3)
+        food = [self.food1, self.food2]
+        self.pub = Pub("The Prancing Pony", 100.00, ["beer", "cider"], food)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
@@ -20,25 +24,25 @@ class TestPub(unittest.TestCase):
     #     self.assertEqual(102.50, self.pub.till)
 
     def test_sell_drink_legal(self):
-        drink = Drink("Tennents", 2.50)
+        drink = Drink("Tennents", 2.50, 4.5)
         customer = Customer("Jimmy", 30, 21, 1)
         self.pub.sell_drink(drink, customer)
         self.assertEqual(102.50, self.pub.till)
 
     def test_sell_drink_illegal(self):
-        drink = Drink("Tennents", 2.50)
+        drink = Drink("Tennents", 2.50, 4.5)
         customer = Customer("Jimmy", 30, 17, 0)
         self.pub.sell_drink(drink, customer)
         self.assertEqual(100.00, self.pub.till)
 
     def test_sell_drink_if_not_drunk(self):
-        drink = Drink("Tennents", 2.50)
+        drink = Drink("Tennents", 2.50, 4.5)
         customer = Customer("Gregg", 30, 26, 0)
         self.pub.sell_drink(drink, customer)
         self.assertEqual(102.50, self.pub.till) 
 
     def test_sell_drink_if_drunk(self):
-        drink = Drink("Tennents", 2.50)
+        drink = Drink("Tennents", 2.50, 4.5)
         customer = Customer("Gregg", 30, 26, 10)
         self.pub.sell_drink(drink, customer)
-        self.assertEqual(100.00, self.pub.till)    
+        self.assertEqual(100.00, self.pub.till)
